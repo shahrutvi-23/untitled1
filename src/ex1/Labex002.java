8923768c51642c61897dd2bcfe40b57e92bee047
shahrutvi-23 <rutvi.shahrm@gmail.com>
2024-07-28 22:16:08 +0530

    this is my first programme for java basics.

---
+++ src/ex1/Labex002.java
@@ -0,0 +1,14 @@
+package ex1;
+
+public class Labex002
+{
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2/2);
+        System.out.println(2*2);
+
+
+    }
+
+}
